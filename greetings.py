3e5c666a948133b6e6b5f1ca67ec4eb86803ca4c
alexiambagaya <mbagayaalexia@gmail.com>
2024-11-22 14:46:00 -0500

    Created English, French and Spanish Greeting

---
+++ greetings.py
@@ -0,0 +1,8 @@
+def english_greeting():
+    print("Hello World!")
+
+def spanish_greeting():
+    print("¡Hola, mundo!")
+
+def french_greeting():
+    print("Bonjour, le monde!")
---
+++ main.py
@@ -0,0 +1,12 @@
+from greetings import english_greeting, spanish_greeting, french_greeting
+
+def main():
+    print("Welcome to the Collaborative Greetings App!")
+    print("Here are the greetings that we have: ")
+    print("-" *30)
+    english_greeting()
+    spanish_greeting()
+    french_greeting()
+
+if __name__ == "__main__":
+        main()
